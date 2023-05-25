43614fd99032b701fcabddd0d4a13a225be58a57
igor Danchanka <one2pixel@yandex.ru>
2023-05-25 10:10:02 +0300

    change prices

--- src/js/files/startData.js
+++ src/js/files/startData.js
@@ -1,13 +1,13 @@
 const startData = {
-	bank: 5000,
+	bank: 5000, // 1000
 
 	countBet: 50,
 	maxBet: 950,
 
 	prices: {
 		price_1: 50,
-		price_2: 120,
-		price_3: 170
+		price_2: 1200,
+		price_3: 2000
 	}
 
 }
